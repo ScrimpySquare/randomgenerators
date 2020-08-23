7ba63cc490bbb2cd9b1df10cbafabd1711a2db2c
ScrimpySquare <58403140+ScrimpySquare@users.noreply.github.com>
2020-08-23 16:21:49 -0400

    Finished basic string generator

--- stringgen.py
+++ stringgen.py
@@ -5,6 +5,15 @@ LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 def main():
     length = int(input('Enter string length: '))
+    print(generate_string(length))
+
+
+def generate_string(length):
+    text = ''
+    for i in range(length):
+        index = random.randint(0, len(LETTERS) - 1)
+        text += LETTERS[index]
+    return text
 
 
 if __name__ == '__main__':
