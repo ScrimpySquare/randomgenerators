373da979bdc99673882fd273196122d03d27f3ac
ScrimpySquare <58403140+ScrimpySquare@users.noreply.github.com>
2020-08-23 15:59:07 -0400

    Initial commit

---
+++ stringgen.py
@@ -0,0 +1,11 @@
+import random
+
+LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+
+
+def main():
+    length = int(input('Enter string length: '))
+
+
+if __name__ == '__main__':
+    main()
