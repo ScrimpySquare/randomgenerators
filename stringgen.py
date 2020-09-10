a9468a98630f814fbcf147cfc91ec68dfd3eb44a
ScrimpySquare <ccandreana6078@gmail.com>
2020-09-10 13:30:09 -0400

    Added numbers to the symbol set

--- stringgen.py
+++ stringgen.py
@@ -1,18 +1,25 @@
 import random
+import time
+import string
 
-LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
+SYMBOLS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 
 
 def main():
     length = int(input('Enter string length: '))
+    start_time = time.time()
     print(generate_string(length))
+    total_time = time.time() - start_time
+    print(f'time: {round(total_time, 3)} seconds')
 
 
 def generate_string(length):
     text = ''
+
     for i in range(length):
-        index = random.randint(0, len(LETTERS) - 1)
-        text += LETTERS[index]
+        index = random.randint(0, len(SYMBOLS) - 1)
+        text += SYMBOLS[index]
+
     return text
 
 
